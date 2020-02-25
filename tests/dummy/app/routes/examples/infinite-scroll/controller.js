15d9c89a0a17efb361c5c948e83b20fad275cb52
Alex Ford <flexyford@users.noreply.github.com>
2020-02-25 09:45:24 -0700

    [bugfix] pin-point bug for ios reproduction
    https://github.com/html-next/vertical-collection/issues/254
    
    never load above again after fetching the second time this allows us
    to pinpoint our webkit bug which occurs on the second fetch in the
    demo application

--- tests/dummy/app/routes/examples/infinite-scroll/controller.js
+++ tests/dummy/app/routes/examples/infinite-scroll/controller.js
@@ -3,19 +3,36 @@ import getNumbers from 'dummy/lib/get-numbers';
 
 export default Controller.extend({
 
+  count: 0,
   numImages: 5,
-
   someProperty: 50,
+
+  init() {
+    this._super(...arguments);
+    this.set('count', 0);
+  },
+
+  loadAbove() {
+    let first = this.get('model.first');
+    let numbers = getNumbers(first - 20, 20);
+    let model = this.get('model.numbers');
+    model.unshiftObjects(numbers);
+    // this.set('model.numbers', newModel);
+    this.set('model.first', first - 20);
+  },
 
   actions: {
 
     loadAbove() {
-      let first = this.get('model.first');
-      let numbers = getNumbers(first - 20, 20);
-      let model = this.get('model.numbers');
-      model.unshiftObjects(numbers);
-      // this.set('model.numbers', newModel);
-      this.set('model.first', first - 20);
+      this.loadAbove();
+      this.incrementProperty('count');
+
+      // never load above again after fetching the second time
+      // this allows us to pinpoint our webkit bug which occurs
+      // on the second fetch in the demo application
+      if (this.get('count') === 2) {
+        this.set('loadAbove', function() {});
+      }
     },
 
     loadBelow() {
